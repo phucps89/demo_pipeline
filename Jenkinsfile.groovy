c95cd36d68b9c385488010e30ac754e2a2f53a0b
Phuc Tran <phuc.tran@nfq.asia>
2021-03-05 23:50:44 +0700

    aa

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,13 +11,15 @@ pipeline {
         SSH_INFO="logisoft@14.225.5.109"
     }
 
-    stage('Example') {
-        sshagent (credentials: ['ssh-logisoft']) {
-            sh """
-                ssh -o StrictHostKeyChecking=no -vv ${SSH_INFO} "
-                    docker ps -a
-                "
-            """
+    stages {
+        stage('Example') {
+            sshagent (credentials: ['ssh-logisoft']) {
+                sh """
+                    ssh -o StrictHostKeyChecking=no -vv ${SSH_INFO} "
+                        docker ps -a
+                    "
+                """
+            }
         }
     }
 }
