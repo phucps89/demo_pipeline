cd4fa507d7cff7ef351bac0c5431af661737b35d
Phuc Tran <phuc.tran@nfq.asia>
2021-03-05 23:09:54 +0700

    aaa

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,10 +3,8 @@ node {
         sh 'ssh -o StrictHostKeyChecking=no -l logisoft 14.225.5.109 uname -a'
     }
     stage('Example') {
-        if (env.BRANCH_NAME == 'main') {
-            echo 'I only execute on the master branch'
-        } else {
-            echo 'I execute elsewhere'
-        }
+        sh '''
+            docker ps -a
+        '''
     }
 }
