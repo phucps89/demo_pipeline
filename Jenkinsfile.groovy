9fe200ca7fd50b569ad7e441a330268823a6f195
Phuc Tran <phuc.tran@nfq.asia>
2021-03-05 23:09:23 +0700

    aa

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,7 @@
 node {
+    sshagent (credentials: ['ssh-logisoft']) {
+        sh 'ssh -o StrictHostKeyChecking=no -l logisoft 14.225.5.109 uname -a'
+    }
     stage('Example') {
         if (env.BRANCH_NAME == 'main') {
             echo 'I only execute on the master branch'
